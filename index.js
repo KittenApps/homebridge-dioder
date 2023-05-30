1bfa8cd4cf4bb4ea0a9d40d03b8891017ed59fca
Silizia <66546362+KittenApps@users.noreply.github.com>
2023-05-30 18:22:01 +0200

    rgb support

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ module.exports = (api) => {
 class DioderAccessoryPlugin {
   constructor(log, config, api) {
     this.log = log;
-    this.config = config;
-    this.api = api;
-
-    this.Service = this.api.hap.Service;
-    this.Characteristic = this.api.hap.Characteristic;
+    this.Characteristic = api.hap.Characteristic;
 
     this.rPin = new Gpio(config['rPin'], {mode: Gpio.OUTPUT});
     this.gPin = new Gpio(config['gPin'], {mode: Gpio.OUTPUT});
@@ -22,71 +18,75 @@ class DioderAccessoryPlugin {
     this.rPin.pwmWrite(0);
     this.gPin.pwmWrite(0);
     this.bPin.pwmWrite(0);
+    this.log("PWM frequency:", this.rPin.getPwmFrequency());
 
-    this.informationService = new this.api.hap.Service.AccessoryInformation()
-      .setCharacteristic(this.api.hap.Characteristic.Manufacturer, "Silizia")
-      .setCharacteristic(this.api.hap.Characteristic.Model, "Fancy LED");
+    this.informationService = new api.hap.Service.AccessoryInformation()
+      .setCharacteristic(this.Characteristic.Manufacturer, "Silizia")
+      .setCharacteristic(this.Characteristic.Model, "Fancy LED");
 
-    this.LEDservice = new this.Service.Lightbulb(this.name);
-    this.LEDservice.getCharacteristic(this.Characteristic.On)
-      .onGet(this.getOn.bind(this))
-      .onSet(this.setOn.bind(this));
-    this.LEDservice.getCharacteristic(this.Characteristic.Brightness)
-      .onGet(this.getBrightness.bind(this))
-      .onSet(this.setBrightness.bind(this));  
+    this.LEDservice = new api.hap.Service.Lightbulb(this.name);
+    this.LEDservice.getCharacteristic(this.Characteristic.On).onGet(this.getOn.bind(this)).onSet(this.setOn.bind(this));
+    this.LEDservice.getCharacteristic(this.Characteristic.Brightness).onGet(this.getBrightness.bind(this)).onSet(this.setBrightness.bind(this));
+    this.LEDservice.getCharacteristic(this.Characteristic.Hue).onGet(this.getHue.bind(this)).onSet(this.setHue.bind(this));
+    this.LEDservice.getCharacteristic(this.Characteristic.Saturation).onGet(this.getSaturation.bind(this)).onSet(this.setSaturation.bind(this));
   }
 
   getServices() {
     return [this.informationService, this.LEDservice];
   }
 
-  async getOn() {
-    return this.rPin.getPwmDutyCycle() > 0;
+  getCharacteristic(c) {
+    return this.LEDservice.getCharacteristic(this.Characteristic[c]).value;
+  }
+
+  getOn() {
+    return this.rPin.getPwmDutyCycle() > 0 || this.gPin.getPwmDutyCycle() > 0 || this.bPin.getPwmDutyCycle() > 0;
   }
   
-  async setOn(value) {
-    const v = Math.floor(this.LEDservice.getCharacteristic(this.Characteristic.Brightness).value * 2.55);
-    this.log(v);
-    this.rPin.pwmWrite(value ? (v || 255) : 0);
+  setOn(on) {
+    // ToDo: Do we need to updateCharacteristics here?
+    if (on){
+      this.setHSV({ h: this.getCharacteristic('Hue'), s: this.getCharacteristic('Saturation'), v: this.getCharacteristic('Brightness') || 100 });
+    } else {
+      this.rPin.pwmWrite(0);
+      this.gPin.pwmWrite(0);
+      this.bPin.pwmWrite(0);
+    }
   }
 
-  async setBrightness(value) {
-    this.rPin.pwmWrite(Math.floor(value * 2.55));
+  setBrightness(v) {
+    this.setHSV({ h: this.getCharacteristic('Hue'), s: this.getCharacteristic('Saturation'), v});
   }
 
-  async getBrightness() {
-    return Math.floor(this.rPin.getPwmDutyCycle() / 2.55);
+  getBrightness() {
+    return this.getHSV().v;
+  }
+
+  setHue(h) {
+    this.setHSV({ h, s: this.getCharacteristic('Saturation'), v: this.getCharacteristic('Brightness')});
+  }
+
+  getHue() {
+    return this.getHSV().h;
+  }
+
+  setSaturation(s) {
+    this.setHSV({ h: this.getCharacteristic('Hue'), s, v: this.getCharacteristic('Brightness')});
+  }
+
+  getSaturation() {
+    return this.getHSV().s;
+  }
+
+  setHSV(c) {
+    // ToDo: Color Correction
+    const { r, g, b } = colord(c).toRgb();
+    this.rPin.pwmWrite(r);
+    this.gPin.pwmWrite(g);
+    this.bPin.pwmWrite(b);
+  }
+
+  getHSV() {
+    return colord({ r: this.rPin.getPwmDutyCycle(), g: this.gPin.getPwmDutyCycle(), b: this.bPin.getPwmDutyCycle() }).toHsv();
   }
 }
-
-/*toggleState() {
-    this.log("toggleState");
-    const on = this.LEDservice.getCharacteristic(this.Characteristic.On).value(); 
-    this.log(on);
-    if(!on){
-      this.log(21);
-      this.updateRGB({ r: 0, g: 0, b: 0 });
-      this.log("turned off", this.LEDservice.getCharacteristic(this.Characteristic.On).value());
-      return;
-    }
-    this.log(42);
-    this.log(this.LEDservice.getCharacteristic(this.Characteristic.Brightness));
-    const v = this.LEDservice.getCharacteristic(this.Characteristic.Brightness).value;
-    this.log("v:", v);
-    if(v != 0){
-      const h = this.LEDservice.getCharacteristic(this.Characteristic.Hue).value;
-      const s = this.LEDservice.getCharacteristic(this.Characteristic.Saturation).value;
-      this.updateRGB(colord({ h, s, v }).toRgb());
-      this.log(h,s,v);
-    } else{
-      this.updateRGB({ r: 0, g: 0, b: 0 });
-    }
-  }
-
-  updateRGB(c){
-    this.log(`Setting rgb values to: Red: ${c.r} Green: ${c.g} Blue: ${c.b}`);
-    this.rPin.pwmWrite(c.r);
-    this.gPin.pwmWrite(c.g);
-    this.bPin.pwmWrite(c.b);
-  }
-}*/
